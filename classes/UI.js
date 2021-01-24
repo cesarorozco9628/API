353ab504c70f1881630feb90a793bd83e0b98fd3
Jorge Araque <araquecjorge@gmail.com>
2021-01-24 18:40:37 -0500

    cambios frond

--- classes/UI.js
+++ classes/UI.js
@@ -1,4 +1,3 @@
-
 export default class UI {
     static printCategories(categories) {
         const container = document.getElementById('categories');
@@ -14,28 +13,28 @@ export default class UI {
                 container.innerHTML += `<div class="col-md-4 mt-4">
                                             <div class="card h-100 bg-dark shadow-card">
                                                 <div class="card-body">
-                                                <label>${question.question}</label>
+                                                <label required="required">${question.question}</label>
                                                     
                                                         <div name="respuestas" class="form-check">
-                                                        <input class="form-check-input" name="${question.question}" type="radio" value="${question.incorrect_answers[0]}" id="${question.incorrect_answers[0]}" >
+                                                        <input required="required" class="form-check-input" name="${question.question}" type="radio" value="" id="${question.incorrect_answers[0]}">
                                                         <label class="form-check-label" for="${question.incorrect_answers[0]}">
                                                         ${question.incorrect_answers[0]}
                                                         </label>
                                                         </div>
                                                         <div class="form-check">
-                                                        <input class="form-check-input" name="${question.question}" type="radio" value="${question.correct_answer} id="${question.correct_answer}">
+                                                        <input required="required" class="form-check-input" name="${question.question}" type="radio" value="" id="${question.correct_answer}">
                                                         <label class="form-check-label" for="${question.correct_answer}">
                                                         ${question.correct_answer}
                                                         </label>
                                                         </div>
                                                         <div class="form-check">
-                                                        <input class="form-check-input" name="${question.question}" type="radio" value="${question.incorrect_answers[2]} id="${question.incorrect_answers[2]}">
+                                                        <input required="required" class="form-check-input" name="${question.question}" type="radio" value="" id="${question.incorrect_answers[2]}">
                                                         <label class="form-check-label" for="${question.incorrect_answers[2]}">
                                                         ${question.incorrect_answers[2]}
                                                         </label>
                                                         </div>
                                                         <div class="form-check">
-                                                        <input class="form-check-input" name="${question.question}" type="radio" value="${question.incorrect_answers[1]} id="${question.incorrect_answers[1]}">
+                                                        <input required="required" class="form-check-input" name="${question.question}" type="radio" value="" id="${question.incorrect_answers[1]}">
                                                         <label class="form-check-label" for="${question.incorrect_answers[1]}">
                                                         ${question.incorrect_answers[1]}
                                                         </label>
@@ -51,18 +50,17 @@ export default class UI {
         else {
             questions.forEach((question) => {
                 container.innerHTML += `<div class="col-md-4 mt-4">
-                                            <div class="card h-100">node 
+                                            <div class="card h-100 bg-dark shadow-card"> 
                                                 <div class="card-body" >
                                                     <label>${question.question}</label>
-                                                        required="required"
                                                             <div class="form-check">
-                                                                <input  onclick=" GetAnswers()" class="form-check-input" name="${question.question}" type="radio" id="checkbox${question.incorrect_answers[0]}">
+                                                                <input required="required" class="form-check-input" name="${question.question}" type="radio" id="checkbox${question.incorrect_answers[0]}">
                                                                 <label class="form-check-label" for="${question.incorrect_answers[0]}">
                                                                         ${question.incorrect_answers[0]}
                                                                 </label>
                                                             </div>
                                                             <div class="form-check">
-                                                                <input onclick=" GetAnswers()" class="form-check-input" name="${question.question}" type="radio" id="checkbox${question.correct_answer}">
+                                                                <input required="required" class="form-check-input" name="${question.question}" type="radio" id="checkbox${question.correct_answer}">
                                                                 <label class="form-check-label" for="${question.correct_answer}">
                                                                         ${question.correct_answer}
                                                                 </label>
@@ -73,31 +71,17 @@ export default class UI {
             })
          };
          
-         let allAnswers = [];
-         questions.forEach(resp => {
-             allAnswers.push(resp.correct_answer);
-         })
-        return  allAnswers;
 
         
     }
-
-    static getAnswers() {
-        const allAnswer =  this.printQuestions; 
+        static getAnswers() {
         let LaRespuesta = document.querySelectorAll(".form-check-input:checked");
-        let print = document.getElementById('print');
-        let count =0;
-        for(let i =0; i< LaRespuesta.length; i++){
-            for(let j =0; j < allAnswer.length ; j++){
-                if(LaRespuesta[i] === allAnswer[j]){
-
-                    count += 1;
-                }
+        LaRespuesta.forEach((question) => {
+            if (question.checked) {
+                question.checked = false 
             }
-        }
-        print.innerHTML = `<div class="col-md-4">
-                              <p>Las respuestas correctas son: </p>
-                           </div>`;
+        })
+        console.log(LaRespuesta);
     }
-    
+
 }
